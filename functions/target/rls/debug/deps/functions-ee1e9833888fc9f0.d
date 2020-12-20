2a996a3f50c8f3a8616a1aaea42308d08a869629
RyosukeNAKATA <59600727+RyosukeNAKATA@users.noreply.github.com>
2020-12-20 17:08:51 +0900

    Hello functions

---
+++ functions/target/rls/debug/deps/functions-ee1e9833888fc9f0.d
@@ -0,0 +1,5 @@
+/Users/ryosuke/rust_projects/functions/target/rls/debug/deps/functions-ee1e9833888fc9f0.rmeta: src/main.rs
+
+/Users/ryosuke/rust_projects/functions/target/rls/debug/deps/functions-ee1e9833888fc9f0.d: src/main.rs
+
+src/main.rs:
